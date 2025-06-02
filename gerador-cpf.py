9c545889d3ec7d1d96e7e7f6e9f600fd9137060a
2506roma <romariodiogo2021@gmail.com>
2025-06-02 20:49:52 -0300

    Passo a Passo Gerador de CPFs

---
+++ gerador-cpf.py
@@ -0,0 +1,7 @@
+# Criar um gerador de CPF
+# tecnologias usadas 
+# Gerador de numero com 11 numeros
+# CPF com 0 no inicio descartar 
+# Comparar com numeros ja criados 
+# Descartar Combinações do mesmo CPF
+
