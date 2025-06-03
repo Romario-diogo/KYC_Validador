f8b5b974381a9a58972b46d02935f9239aff28c4
2506roma <romariodiogo2021@gmail.com>
2025-06-02 21:48:43 -0300

    Gerador CPFs Comcluido Par 1

--- gerador-cpf.py
+++ gerador-cpf.py
@@ -1,14 +1,29 @@
 # Criar um gerador de CPF
 # tecnologias usadas 
 import random
+
+usado = set()
+
 # Gerador de numero com 11 numeros
-numero = random.choices([0,1,2,3,4,5,6,7,8,9], k=11)
+while True:
+    numero= random.choices(range(10),k=11)
 
 # CPF com 0 no inicio descartar 
-if numero[0] == 0:
-    numero = random.choices([0,1,2,3,4,5,6,7,8,9], k=11)
-else:
-    print(numero)
+    if numero[0] == 0:
+        continue
+
+    numero_str = ''.join(map(str, numero))
+
+    if numero_str not in usado:
+        usado.add(numero_str)
+        print("CPF Gerado ", numero_str)
+        # Formata no pafrão 000.000.000-00
+        cpf = f"{numero_str[:3]}.{numero_str[3:6]}.{numero_str[6:9]}-{numero_str[9:]}"
+
+        print(f"CPFs: {cpf}")
+
+        break
+
 # Criar um padrão de CPF.
 # Comparar com numeros ja criados 
 # Descartar Combinações do mesmo CPF
