647d519f048135f7f48dd53d65c43c02045fdf4c
2506roma <romariodiogo2021@gmail.com>
2025-06-02 21:03:00 -0300

    Criando CPFs  e Del inciais com 0

--- gerador-cpf.py
+++ gerador-cpf.py
@@ -1,7 +1,15 @@
 # Criar um gerador de CPF
 # tecnologias usadas 
+import random
 # Gerador de numero com 11 numeros
+numero = random.choices([0,1,2,3,4,5,6,7,8,9], k=11)
+
 # CPF com 0 no inicio descartar 
+if numero[0] == 0:
+    numero = random.choices([0,1,2,3,4,5,6,7,8,9], k=11)
+else:
+    print(numero)
+# Criar um padrão de CPF.
 # Comparar com numeros ja criados 
 # Descartar Combinações do mesmo CPF
 
